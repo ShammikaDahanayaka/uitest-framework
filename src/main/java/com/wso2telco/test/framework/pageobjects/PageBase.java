697dfa84069bdf3b4cd728dcb8e90ff9cfa906b9
maheshcode <mahesh@wso2telco.com>
2017-06-02 12:23:54 +0530

    shift to iframe method

--- src/main/java/com/wso2telco/test/framework/pageobjects/PageBase.java
+++ src/main/java/com/wso2telco/test/framework/pageobjects/PageBase.java
@@ -1,5 +1,7 @@
 package com.wso2telco.test.framework.pageobjects;
 
+import com.wso2telco.test.framework.core.WebPelement;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import com.wso2telco.test.framework.core.PageActions;
 import com.wso2telco.test.framework.core.PrePageBase;
@@ -7,14 +9,28 @@ import com.wso2telco.test.framework.core.PrePageBase;
 public class PageBase extends PrePageBase implements PageActions {
 
 	
-	
+	Logger logger = Logger.getLogger(PageBase.class);
 	
 	public boolean flag;
 
 	public PageBase(WebDriver driver) {
 		super(driver);
 		//pageActions = new PrePageBase(driver);
-		
+
+	}
+
+   public void switchtoFrame(String frameName){
+	   logger.debug("moving to default content");
+		driver.switchTo().defaultContent();
+	   logger.info("moving to frame " + frameName);
+	   driver.switchTo().frame(frameName);
+   }
+
+
+	public void switchtoDefaultContent(){
+
+		logger.debug("moving to default content");
+		driver.switchTo().defaultContent();
 	}
 
 }
